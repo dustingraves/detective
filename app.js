4748299399412d167acf5187f30b09fa1b2e728f
Dustin Graves <dustin.graves@concur.com>
2015-11-03 03:56:47 -0800

    commiting latest code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var getFile = require('./getFile');
 var _ = require('underscore');
 var investigation = getFile.processArgs(process);
 
-//var pathDict = {paths:[]};
+var internal = exports.internal = {};
 
 var graph = _.reduce(investigation, addToGraph, {});
 
@@ -21,6 +21,7 @@ function addToGraph(acc, witnessAccount) {
         if (!_.has(acc, event)) {
             // add vertice
             acc[event] = [];
+
         }
         if (previousEvent) {
             // add edge
@@ -29,185 +30,55 @@ function addToGraph(acc, witnessAccount) {
         previousEvent = event;
     }
 }
-var endpoints = startOrEnd(graph);
 
+//Find starts and ends
+var path = rootEnd();
 
-
-function startOrEnd(graph){
-    var endpoints = {start:[], end:[]};
-    for (var v in graph) {
-       if(checkStart(v)){
-           endpoints.start.push(v);
-       }else if(graph[v].length == 0){
-           endpoints.end.push(v);
-       }
-    }
-    return endpoints;
-
-    function checkStart(v){
-        for (w in graph){
-            if(_.contains(graph[w], v)){
-                return false;
-            }
+function rootEnd(){
+    var found = {start: [], end:[]};
+    _.each(graph, function(e, v){
+        if (e.length === 0){
+            found.end.push(v);
+        } else if (checkStart(v)){
+            found.start.push(v);
         }
-        return true;
-    }
-}
-//////////////////////
-
-//followPath(endpoints, graph);
-//console.log();
-
-
-
-
-function findBackwardPath(endpoints,graph){
-    for(key in graph){
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////
-followNewPath(endpoints, graph);
-
-function followNewPath(endpoints, graph) {
-    var paths = [];
-    var seen = [];
-    _.each(endpoints.start, function(start){
-        seenPath = [];
-        _.each(endpoints.end, function(end){
-            //console.log(seen);
-            visit(start, end, [], seen, graph);
-            console.log(seenPath);
-            //console.log(output);
-
-            function visit(node, end, path, seen, graph){
-
-                //seen = seen == undefined ? [] : seen;
-
-                if (node === end){
-                    // Target reached
-                    path.push(node);
-                    seenPath.push(seen);
-                    paths.push(path);
-                    return path;
-                }
-
-                //Mark the node
-                seen.push(node);
-
-                //Try all unmarked neighbors
-                _.each(graph[node], addNode);
-                function addNode(w){
-                    if(!_.has(path, node)){
-                        //add to path
-                        //path.push(node);
-                    }
-                    if(!_.has(seen, w)){
-
-                        path.push(w);
-                        visit(w, end, path, seen, graph);
-                    }
-                }
-            }
-        });
     });
 
-    return paths;
+    return found;
+
+    function checkStart(v) {
+        var start = true;
+        _.each(graph, function(e){
+            if(_.contains(e, v)){
+                start = false;
+            }
+        });
+        return start;
+    }
 }
 
+//Find all paths between start and end
+var findAllPaths = function(graph, start, end, path){
+    path.push(start);
+    console.log(path);
+    if (start === end){
+        return [path];
+    }
+    if (!graph.hasOwnProperty(start)){
+        return [];
+    }
+    var paths = [];
+    for (var i=0; i<graph[start].length; i++) {
+        if (graph.hasOwnProperty(start) && path.indexOf(graph[start][i]) == -1){
+            var newPaths = findAllPaths(graph, graph[start][i], end, path);
+            for(var n=0; n<newPaths.length; n++){
+                paths.push(newPaths[n]);
+            }
+        }
 
-/////////////////////
+    }
+    return paths;
+};
 
-//Other Stuff I was working on
-
-//
-//for (v in graph){
-//    if (graph[v] == null){
-//        endpoint.push(v);
-//    }
-
-//
-//}
-
-
-//console.log(visited.join(', '));
-
-////This section follows the edges from the root and creates the path arrays
-//function findPaths(graph) {
-//    for (var v in graph){
-//
-//        for(var i=0;i<graph[v].length; i++){
-//            if(touchedDict.seen.indexOf(v) == -1){
-//                pathDict.paths.push(startPath(v, i));
-//            }
-//        }
-//    }
-//}
-//
-////function getNextKey(){
-////    //var localCount = count != null ? count : object.length;
-////    var key = graphDictArray.shift();
-////    if (validateKey(key, graph)) {
-////        return key;
-////    }
-////}
-//
-//function validateKey(key, object){
-//   return object.hasOwnProperty(key);
-//}
-//
-//function startPath(key, edgeCount) {
-//    var localPath = [];
-//    localPath.push(key);
-//    touchedDict.starts.push(key);
-//    followPath(graph[key][edgeCount], localPath);
-//
-//    return localPath;
-//}
-//
-//
-//function followPath(edge, localPath, forks) {
-//
-//    if(graph[edge] == null && forks == true){
-//
-//        findPaths(graph);
-//    }
-//
-//    localPath.push(edge);
-//
-//    touchedDict.seen.push(edge);
-//
-//    if (graph[edge] != null) {
-//        if (graph[edge].length > 0){
-//            var splits = true;
-//        }else{
-//            touchedDict.ends.push(edge);
-//        }
-//        for (var i = 0; i < graph[edge].length; i++) {
-//            if(touchedDict.seen.indexOf(graph[edge][i]) == -1 && i+1 <graph[edge].length){
-//                i++;
-//            }
-//            followPath(graph[edge][i], localPath, splits);
-//        }
-//    }
-//}
-
-
-console.log(graph);
-//console.log(pathDict.paths);
-//console.log(touchedDict);
-//console.log(graphDict);
-//console.log(graphDictArray);
+console.log(findAllPaths(graph, path.start[0], path.end[0], []));
+//console.log(graph);
