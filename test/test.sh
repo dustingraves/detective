e35377d47e8e6c52ceaff4848b5c837e4e710268
Dustin Graves <dustin.graves@concur.com>
2015-11-06 13:31:02 -0800

    Fixing CircleCI Post Tests

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-env NODE_ENV=run
-node app.js ../samples/events.json
-node app.js ../samples/events2.json
-node app.js ../samples/events3.json
-node app.js ../samples/events4.json
+NODE_ENV=run
+
+echo -e '\nFull Merge:'
+node ../app.js ../samples/events.json
+
+echo -e '\nPartial Merge:'
+node ../app.js ../samples/events2.json
+
+echo -e '\nNo Merge:'
+node ../app.js ../samples/events3.json
+
+echo -e '\nFull Merge with Multiple Unions:'
+node ../app.js ../samples/events4.json
